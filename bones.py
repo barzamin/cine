c5ed9722ac023a787680ce169f265044296a914b
erin petra sofiya moon <erin@hecke.rs>
2024-04-11 01:24:49 -0400

    woooooo formatting

--- bones.py
+++ bones.py
@@ -16,33 +16,37 @@ mem = DOLMemory()
 
 player_slots = 0x80453080
 slot = 0
-p_StaticPlayer = player_slots + 0xe90 * slot
+p_StaticPlayer = player_slots + 0xE90 * slot
 
 p_p_Fighter_GObj = p_StaticPlayer + 0xB0
-p_Fighter_GObj = mem.readv(p_p_Fighter_GObj, 'I')
-p_Fighter = mem.readv(p_Fighter_GObj + 0x2c, 'I')
+p_Fighter_GObj = mem.readv(p_p_Fighter_GObj, "I")
+p_Fighter = mem.readv(p_Fighter_GObj + 0x2C, "I")
 
 
 ## figure out how many parts there are in the bone table
-fighter_kind = mem.readv(p_Fighter + 0x4, 'I')
+fighter_kind = mem.readv(p_Fighter + 0x4, "I")
 # FighterPartsTable** ftPartsTable
 # comes from PlCo.dat
 # indexed by fighter_kind
-ftPartsTable = mem.readv(0x804D6544, 'I')
+ftPartsTable = mem.readv(0x804D6544, "I")
 # typedef struct _FighterPartsTable {
 #     u8* joint_to_part;
 #     u8* part_to_joint;
 #     u32 parts_num;
 # } FighterPartsTable;
-fighter_ftParts = mem.readv(ftPartsTable + fighter_kind * 4, 'I')
-parts_num = mem.readv(fighter_ftParts + 0x8, 'I')
+fighter_ftParts = mem.readv(ftPartsTable + fighter_kind * 4, "I")
+parts_num = mem.readv(fighter_ftParts + 0x8, "I")
 
 ## bone table
 # FighterBone[]
 # sizeof(FighterBone) == 0x10
-p_bone_table = mem.readv(p_Fighter + 0x5e8, 'I')
+p_bone_table = mem.readv(p_Fighter + 0x5E8, "I")
 
-parts = [FighterBone.from_bytes(mem.read(p_bone_table + idx*0x10, 0x10)) for idx in range(parts_num)]
+parts = [
+    FighterBone.from_bytes(mem.read(p_bone_table + idx * 0x10, 0x10))
+    for idx in range(parts_num)
+]
+
 
 @dataclass
 class JObj:
@@ -54,17 +58,16 @@ class JObj:
 
     @classmethod
     def from_mem(cls, p_jobj):
-        flags = JObj_Flags(mem.readv(p_jobj + 0x14, 'I'))
-        rotate = mem.readnp32(p_jobj + 0x1c, (4,)) # Quaternion
-        scale = mem.readnp32(p_jobj + 0x2c, (3,)) # Vec3
-        translate = mem.readnp32(p_jobj + 0x38, (3,)) # Vec3
-        mtx = mem.readnp32(p_jobj + 0x44, (3,4)) # Mtx
+        flags = JObj_Flags(mem.readv(p_jobj + 0x14, "I"))
+        rotate = mem.readnp32(p_jobj + 0x1C, (4,))  # Quaternion
+        scale = mem.readnp32(p_jobj + 0x2C, (3,))  # Vec3
+        translate = mem.readnp32(p_jobj + 0x38, (3,))  # Vec3
+        mtx = mem.readnp32(p_jobj + 0x44, (3, 4))  # Mtx
 
-        return cls(flags=flags,
-                   rotate=rotate,
-                   scale=scale,
-                   translate=translate,
-                   mtx=mtx)
+        return cls(
+            flags=flags, rotate=rotate, scale=scale, translate=translate, mtx=mtx
+        )
+
 
 # def dump_jobj(p_jobj, depth=0):
 #     idt = '  ' * depth
@@ -74,22 +77,24 @@ class JObj:
 #     print(idt + f' |- {scale=}')
 #     print(idt + f' |- {translate=}')
 
+
 def walk_jobj(p_jobj, depth=0):
     dump_jobj(p_jobj, depth=depth)
 
-    p_child = mem.readv(p_jobj + 0x10, 'I')
+    p_child = mem.readv(p_jobj + 0x10, "I")
     if p_child != 0:
-        dump_jobj(p_child, depth=depth+1)
+        dump_jobj(p_child, depth=depth + 1)
 
-    p_next = mem.readv(p_jobj + 0x08, 'I')
+    p_next = mem.readv(p_jobj + 0x08, "I")
     if p_next != 0:
         dump_jobj(p_next, depth=depth)
 
+
 # dump_jobj(parts[0].p_joint)
 for i, bone in enumerate(parts):
-    print(f' --- {i:02} --- ')
+    print(f" --- {i:02} --- ")
     jobj = JObj.from_mem(bone.p_joint)
-    print(f'{bone.p_joint:08x} {jobj.translate=} {jobj.rotate=}')
+    print(f"{bone.p_joint:08x} {jobj.translate=} {jobj.rotate=}")
 
 
 IPython.embed()
--- util.py
+++ util.py
@@ -1,27 +1,29 @@
 import os
 
+
 class hexdump:
-    def __init__(self, buf, base_addr = 0x0):
+    def __init__(self, buf, base_addr=0x0):
         self.buf = buf
         self.base_addr = base_addr
 
     def __iter__(self):
-        def _printable(x): return 32 <= x < 127 
+        def _printable(x):
+            return 32 <= x < 127
 
-        BPL = 16 # 16 bytes per line
+        BPL = 16  # 16 bytes per line
         last_line, last_bts = None, None
         for i in range(0, len(self.buf), BPL):
-            bts = bytes(self.buf[i: i+BPL])
+            bts = bytes(self.buf[i : i + BPL])
 
             if bts == last_bts:
-                line = '*'
+                line = "*"
             else:
                 # 23 = 8*2 + (8-1)
-                line = '{:08x}  {:23}  {:23}  |{:16}|'.format(
+                line = "{:08x}  {:23}  {:23}  |{:16}|".format(
                     self.base_addr + i,
-                    ' '.join(f'{x:02x}' for x in bts[:8]),
-                    ' '.join(f'{x:02x}' for x in bts[8:]),
-                    ''.join(chr(x) if _printable(x) else '.' for x in bts)
+                    " ".join(f"{x:02x}" for x in bts[:8]),
+                    " ".join(f"{x:02x}" for x in bts[8:]),
+                    "".join(chr(x) if _printable(x) else "." for x in bts),
                 )
 
             if line != last_line:
@@ -30,7 +32,7 @@ class hexdump:
             last_line, last_bts = line, bts
 
     def __str__(self):
-        return '\n'.join(self)
+        return "\n".join(self)
 
     def __repr__(self):
         return str(self)
